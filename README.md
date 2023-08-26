9690603e122462df464122b6f40ee72f01bb49db
Wishmastering <charsdb@gmail.com>
2023-08-26 17:12:53 -0600

    fetching on its way

--- README.md
+++ README.md
@@ -11,23 +11,39 @@ APIs a usar:
 
 Requerimientos ->
 
-- Necesita mostrar un input para buscar la pelicula y un boton para buscar.
+✅ Necesita mostrar un input para buscar la pelicula y un boton para buscar.
 
-- Lista las peliculas encontradas y muestra el titulo, a;o y poster
+✅ Lista las peliculas encontradas y muestra el titulo, a;o y poster, NO HAGAS FECTH AUN.
 
-- controlar inputs
+Mas bien, crea un 'mockup' del JSON cuando hay un response 'Positivo' en el endpoint del API en una carpeta de tu VSC y nombrala 'mockup_good_response'
 
-- Manejar errores para ->
+Hacer lo mismo cuando hay un response 'Negativo'
+
+Con estos mocks entonces ahora si, lista las peliculas encontradas
+
+✅ Crea un condicional si no hay peliculas, mostrar 'no se encontro resultados para tu busqueda'
+
+✅ Crea un componente para el map de movies
+
+✅ Extrar logica del contrato del API para no depender directamente de sus valores, abstraer a custom hook llamado useMovies
+
+✅ controlar inputs
+
+✅ Manejar errores para ->
 
 a. Si el string es vacio
-b. Si solo hay numeros
+b. Si solo hay numeros (ReGex)
 c. Si es menor a 3 caracteres
 
-- map de lo encontrado
+✅ Desplegar el error en cuestion debajo del buscador de peliculas
 
-- Haz que las peliculas se muestren en un grid responsive.
+✅ En el manejo de error anterior, hacerlo todo en useEffect (En caso de habero hecho con useState) y pasarlo a un custom hook llamado 'useSearch'
 
--- fetch (progressing)
+✅ Manejar el caso donde el primer input es 'vacio' y nos tira un error inmediatamente, en el primer render de la pagina, no queremos que tire este error
+
+✅ Haz que las peliculas se muestren en un grid responsive.
+
+- Realiza el fetch
 
 Primera iteracion:
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,101 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import json from "./mock-response/good-response.json";
+import no_json from "./mock-response/bad-response.json";
+import { useEffect, useState, useRef } from "react";
 
-function App() {
-  return <> Hey </>;
+function useMovies() {
+  const movies = json.Search;
+
+  const mappedMovies = movies?.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+  }));
+  return { movies: mappedMovies };
 }
 
-export default App;
+function useSearch() {
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+  const [searchRef, setSearchRef] = useState(false);
+
+  useEffect(() => {
+    if (searchRef) {
+      (searchRef === search) === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacia");
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar pelicula con solo numeros");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("Pelicula debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, setSearch, error };
+}
+
+export default function App() {
+  const { movies } = useMovies();
+  const { search, setSearch, error } = useSearch();
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h4>Buscador De Peliculas</h4>
+        <form>
+          <input
+            value={search}
+            onChange={handleChange}
+            type="text"
+            placeholder="Avengers, Star Wars, LOTR"
+          />
+          <button> Buscar</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+
+      <main>
+        <Movies movies={movies} />
+      </main>
+    </div>
+  );
+}
+
+function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return (
+    <>
+      {hasMovies ? (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>{movie.year}</p>
+              <img src={movie.poster} alt={movie.title} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h4>No se encontraron peliculas para tu busqueda</h4>
+      )}
+    </>
+  );
+}
